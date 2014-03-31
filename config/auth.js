7712df9a57192fbe764093c4dce86410cf949d15
Ted Jenkins <edwardtjenkins@gmail.com>
2014-03-30 19:44:10 -0600

    Initial commit

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+  'facebookAuth' : {
+    'clientID'        : '220371688171514',
+    'clientSecret'    : '1fc36d3e91a2b5e3cbbf0fe690ac3ad5',
+    'callbackURL'     : 'http://localhost:8080/auth/facebook/callback/'
+  }
+};
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'url': 'mongodb://useruser:userpass@ds039507.mongolab.com:39507/users'
+};
